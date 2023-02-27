b38328a5b5bfba072c7f4327643e4037d3dbcc3b
vadimlol22 <vadimlol22@bk.ru>
2023-02-27 17:46:27 +0300

    first hw

---
+++ 1/src/components/counter/conteiner/CounterConteiner.js
@@ -0,0 +1,57 @@
+import {Component} from 'react';
+import { CounterView } from '../component/CounterView';
+
+class CounterContainer extends Component {
+    constructor(props) {
+        super(props)
+
+
+        this.state = {
+            countValue : 0,
+            isEven : true
+        }
+    }
+
+    componentDidUpdate() {
+        if(this.state.countValue % 2 === 0 && this.state.isEven !== true) {
+            this.setState({isEven : true})
+        } 
+        if (this.state.countValue % 2 !== 0 && this.state.isEven !== false) {
+            this.setState({isEven : false})
+        }
+
+    }
+
+    handleIncrement = () => {   
+        this.setState({countValue : this.state.countValue + 1})
+    }
+
+    handleReset = () => {   
+        this.setState({countValue : 0})
+    }
+
+    handleSubtraction = () => {   
+        this.setState((prevstate) => {
+            if (prevstate.countValue - 1 < 0) {
+                return {countValue : 0 }
+            } else {
+                return {countValue : prevstate.countValue - 1}
+            }
+        })
+    }
+
+
+    render() {
+        
+        return <CounterView 
+        counterValue={this.state.countValue} 
+        oddEven={this.state.isEven} 
+        handleIncrement={this.handleIncrement} 
+        handleReset={this.handleReset}
+        handleSubtraction={this.handleSubtraction}
+        />
+        
+    }
+}
+
+export default CounterContainer
